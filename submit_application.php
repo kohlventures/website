8d7acad81b712f800cbe991b197c3fcfc0134dd5
kohlventures <wk275@cornell.edu>
2024-11-03 02:33:44 -0800

    Update submit_application.php

--- submit_application.php
+++ submit_application.php
@@ -13,27 +13,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $disability_status = htmlspecialchars($_POST['disability_status']);
     $veteran_status = htmlspecialchars($_POST['veteran_status']);
 
-    // Handling resume file upload
-    if (isset($_FILES['resume']) && $_FILES['resume']['error'] == UPLOAD_ERR_OK) {
-        $resume_tmp_path = $_FILES['resume']['tmp_name'];
-        $resume_name = basename($_FILES['resume']['name']);
-        $resume_type = $_FILES['resume']['type'];
-        $resume_content = file_get_contents($resume_tmp_path);
-    } else {
-        echo "Error uploading resume.";
-        exit;
-    }
-
     // Prepare email content
-    $to = "treasuregoblinportal@gmail.com";
+    $to = "treasuregoblinportal@gmail.com"; // Change to your HR email
     $subject = "New Application from $name";
-    $message = "You have received a new application.\n\n" .
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $message = "You have received a new application:\n\n" .
                "Name: $name\n" .
                "Email: $email\n" .
                "Phone: $phone\n" .
                "Location: $location\n" .
                "Position of Interest: $position\n" .
-               "Cover Letter: $cover_letter\n" .
+               "Cover Letter:\n$cover_letter\n" .
                "LinkedIn Profile: $linkedin\n" .
                "Gender: $gender\n" .
                "Hispanic/Latino: $hispanic\n" .
@@ -41,30 +34,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                "Disability Status: $disability_status\n" .
                "Veteran Status: $veteran_status\n";
 
-    // Boundary for attachments
-    $boundary = md5(uniqid(time()));
-
-    // Headers
-    $headers = "From: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-
-    // Message Body with attachment
-    $body = "--$boundary\r\n";
-    $body .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    $body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $body .= $message . "\r\n";
-    
-    // Add the resume attachment
-    $body .= "--$boundary\r\n";
-    $body .= "Content-Type: $resume_type; name=\"$resume_name\"\r\n";
-    $body .= "Content-Disposition: attachment; filename=\"$resume_name\"\r\n";
-    $body .= "Content-Transfer-Encoding: base64\r\n\r\n";
-    $body .= chunk_split(base64_encode($resume_content)) . "\r\n";
-    $body .= "--$boundary--";
-
     // Send the email
-    if (mail($to, $subject, $body, $headers)) {
+    if (mail($to, $subject, $message, $headers)) {
         echo "Application submitted successfully.";
     } else {
         echo "There was an error sending the application. Please try again later.";
